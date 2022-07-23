5ef3ed02d90de02e475a61333ebb826422bb6d4b
Lucy <lzlucyz@gmail.com>
2022-07-24 03:41:06 +1200

    Added new state

--- .history/template v1/frontend/src/components/Shelf_20220724033839.js
+++ .history/template v1/frontend/src/components/Shelf_20220724033839.js
@@ -46,7 +46,7 @@ const Shelf = () => {
 
   const [currentTaskState, setCurrentTaskState] = useRecoilState(taskState);
 
-  const [onDisplay, setOnDisPlay] = useState([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], [11, 11], [12, 12], [13, 13], [14, 14], [15, 15]])
+  const [onDisplay, setOnDisPlay] = useState([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5,5], 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
   const [ShelfItemsCollection, setShelfItemsCollection] = useState([
     4, 2, 1, 0, 1, 3, 4, 15, 15, 15, 15, 15, 15, 15, 15, 15,
