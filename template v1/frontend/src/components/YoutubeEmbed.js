2db1042fb99cc88309ef4cca8dee384640d74770
jyan575 <100386996+jyan575@users.noreply.github.com>
2022-07-24 07:17:55 +1200

    Videos

--- template v1/frontend/src/components/YoutubeEmbed.js
+++ template v1/frontend/src/components/YoutubeEmbed.js
@@ -1,21 +1,13 @@
-import PropTypes from "prop-types";
+import { Player } from 'video-react';
 
-const YoutubeEmbed = ({ embedId }) => (
-    <div className="video-responsive">
-        <iframe
-            width="853"
-            height="480"
-            src={`https://auckland.au.panopto.com/Panopto/Pages/Embed.aspx?id=b02784f7-f51f-426b-95eb-aed700330751${embedId}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-        />
-    </div>
-);
-
-YoutubeEmbed.propTypes = {
-    embedId: PropTypes.string.isRequired
-};
-
-export default YoutubeEmbed;
+export default props => {
+    return (
+        <Player>
+            <source src="https://www.youtube.com/watch?v=0Fcdln0HjBA&ab_channel=DavidWelch" />
+            <link
+                rel="stylesheet"
+                href="https://video-react.github.io/assets/video-react.css"
+            />
+        </Player>
+    );
+}
--- template v1/frontend/src/pages/YoutubeVideo.js
+++ template v1/frontend/src/pages/YoutubeVideo.js
@@ -6,17 +6,18 @@ import YoutubeEmbed from "../components/YoutubeEmbed";
 
 const YoutubeVideo = () => {
 
-    // State for youtube URL -> the thing you put in the brackets in useState() is just the default url it start with (can also be empty "")
-    const [currentURL, setCurrentURL] = useState("insert-default-url-here");
-    // To view the current URL, access it with currentURL
-    // To change the URL playing, use setCurrentURL("insert-new-URL-here");
+  // State for youtube URL -> the thing you put in the brackets in useState() is just the default url it start with (can also be empty "")
+  const [currentURL, setCurrentURL] = useState("insert-default-url-here");
+  // To view the current URL, access it with currentURL
+  // To change the URL playing, use setCurrentURL("insert-new-URL-here");
 
   return (
     <div className="">
       <div className="">
+        <YoutubeEmbed />
 
 
-    </div>
+      </div>
     </div>
   )
 }
