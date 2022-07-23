0189f1f3ff76334e97299f438efc3c53728bbcfd
Lucy <lzlucyz@gmail.com>
2022-07-23 19:14:44 +1200

    drinks

--- .history/template v1/frontend/src/hooks/useDrinksContext_20220723191227.js
+++ .history/template v1/frontend/src/hooks/useDrinksContext_20220723191227.js
@@ -5,7 +5,7 @@ export const useDrinksContext = () => {
   const context = useContext(DrinksContext)
 
   if(!context) {
-    throw Error('useDrinksContext must be used inside a DrinksContentProvider')
+    throw Error('useDrinksContext must be used inside a WorkoutsContextProvider')
   }
 
   return context
--- .history/template v1/frontend/src/pages/Home_20220723191332.js
+++ .history/template v1/frontend/src/pages/Home_20220723191332.js
@@ -2,8 +2,8 @@ import { useEffect } from "react"
 import { useDrinksContext } from "../hooks/useDrinksContext"
 
 // components
-// import DrinkDetails from "../components/DrinkDetails"
-// import DrinkForm from "../components/DrinkForm"
+import DrinkDetails from "../components/DrinkDetails"
+import DrinkForm from "../components/DrinkForm"
 
 const Home = () => {
   const { drinks, dispatch } = useDrinksContext()
@@ -25,10 +25,10 @@ const Home = () => {
     <div className="home">
       <div className="drinks">
         {drinks && drinks.map(drink => (
-          {/* <DrinkDetails drink={drink} key={drink._id} /> */}
+          <DrinkDetails drink={drink} key={drink._id} />
         ))}
       </div>
-      {/* <DrinkForm /> */}
+      <DrinkForm />
     </div>
   )
 }
