41df2a02a9726663cc4aa1a7fcea409593af0fc0
bliu631 <79785035+bliu631@users.noreply.github.com>
2022-07-23 12:41:21 +1200

    made some changes

--- template v1/backend/server.js
+++ template v1/backend/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/drinks', workoutRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
