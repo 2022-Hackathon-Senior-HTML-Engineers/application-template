a1f0117dcb6aef5645e38889d1b8beed99de180e
Lucy <lzlucyz@gmail.com>
2022-07-23 19:11:28 +1200

    history

---
+++ .history/template v1/frontend/src/pages/Home_20220723190340.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react"
+import { useDrinksContext } from "../hooks/useDrinksContext"
+
+// components
+import DrinkDetails from "../components/DrinkDetails"
+import DrinkForm from "../components/DrinkForm"
+
+const Home = () => {
+  const { drinks, dispatch } = useDrinksContext()
+
+  useEffect(() => {
+    const fetchDrinks = async () => {
+      const response = await fetch('/api/drinks')
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_DRINKS', payload: json})
+      }
+    }
+
+    fetchDrinks()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="drinks">
+        {drinks && drinks.map(drink => (
+          <DrinkDetails drink={drink} key={drink._id} />
+        ))}
+      </div>
+      <DrinkForm />
+    </div>
+  )
+}
+
+export default Home
